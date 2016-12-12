747aa4975dcccc17894e52c039c0b0f6d64a8348
Eric Meng (Shang Hai Wei Chuang Ruan Jian) <v-wemen@microsoft.com>
2016-12-12 09:28:14 +0800

    first modify

--- Models/Core/Group.cs
+++ Models/Core/Group.cs
@@ -1,11 +1,13 @@
+using System;
+
 namespace HelloCore.Models.Core
 {
     public class Group
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string CreateTime { get; set; }
+        public DateTime CreateTime { get; set; }
         public byte ValidStatus { get; set; }
     }
 
---
+++ Models/Core/User.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace HelloCore.Models.Core{
+    public class User{
+
+public int Id{get;set;}
+public string Name{get;set;}
+
+public string Description{get;set;}
+
+public string EmailAddress{get;set;}
+
+public string PhoneNumber{get;set;}
+
+public string Url{get;set;}
+
+
+
+    }
+}
