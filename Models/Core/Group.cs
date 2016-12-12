78a54fca9a5a71c007c14b366aad1172a44ecf7b
Eric Meng (Shang Hai Wei Chuang Ruan Jian) <v-wemen@microsoft.com>
2016-12-12 09:01:28 +0800

    init project

---
+++ Models/Core/Group.cs
@@ -0,0 +1,13 @@
+namespace HelloCore.Models.Core
+{
+    public class Group
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CreateTime { get; set; }
+        public byte ValidStatus { get; set; }
+    }
+
+
+}
